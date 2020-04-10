2f110b1b79a3e435d5f1abe980bee5c89c85dc03
Bogdan <elektro739@gmail.com>
2020-04-10 22:05:13 +0300

    Initial commit

---
+++ src/main/java/com/siteonspring/hritsay/blog/controllers/MainController.java
@@ -0,0 +1,23 @@
+package com.siteonspring.hritsay.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Home");
+        return "home";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "About Us");
+        return "about";
+    }
+
+}
